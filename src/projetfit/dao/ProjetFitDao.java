57d9ad54a3570ae0e94fec160ab24b8366728dee
ESIC <ESIC@192.168.1.44>
2019-02-13 15:36:35 +0100

    Ajout du package DAO

---
+++ src/projetfit/dao/ProjetFitDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetfit.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ESIC
+ */
+public class ProjetFitDao {
+    // Configuration de la base de données
+    protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    protected static final String DB_NAME = "projetfit";
+    protected static final String URL = "jdbc:mysql://localhost/" + DB_NAME;
+    protected static final String USER = "root";
+    protected static final String PASSWORD = "";
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
